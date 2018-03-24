e8bc411263b159e1b891949f2154fa480f1aebfc
Michael Müller <mm@konsorten.de>
2018-03-24 23:07:16 +0100

    gulp 4.0
    updated gulp to version 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,9 @@ const uglify        = require('gulp-uglify');
 const concat        = require('gulp-concat');
 const rename        = require('gulp-rename');
 const handlebars    = require('gulp-compile-handlebars');
-const browserSync   = require('browser-sync');
+const browserSync   = require('browser-sync').create();
 const ghPages       = require('gulp-gh-pages');
 const sassGlob      = require('gulp-sass-bulk-import');
-const watch         = require('gulp-watch');
 const babel         = require('gulp-babel');
 const typescript    = require('gulp-typescript');
 const tslint        = require('gulp-tslint');
@@ -40,19 +39,24 @@ var paths = {
   },
 }.init();
 
-gulp.task('serve', () => {
-  browserSync.init({
+function serve() {
+  return browserSync.init({
     server: paths.dist.root,
-    open: false,
+    open: true,
     notify: false,
 
     // Whether to listen on external
     online: false,
   });
-});
+}
+exports.serve = serve;
 
-gulp.task('styles', () => {
-  gulp.src([paths.src.sass])
+/**
+ * Compile sass files into css
+ * 
+ */
+function styles() {
+  return gulp.src([paths.src.sass])
     .pipe(sassGlob())
     .on('error', log)
     .pipe(sass({
@@ -61,73 +65,82 @@ gulp.task('styles', () => {
     .on('error', log)
     .pipe(prefixer('last 2 versions'))
     .on('error', log)
-    .pipe(gulp.dest(paths.dist.css))
-    .pipe(browserSync.reload({stream: true}));
-});
+    .pipe(gulp.dest(paths.dist.css));
+}
+exports.styles = styles;
 
-/*
-* Compile handlebars/partials into html
-*/
-gulp.task('templates', () => {
+/**
+ * Compile handlebars/partials into html
+ * 
+ */
+function templates() {
   var opts = {
     ignorePartials: true,
     batch: ['src/partials'],
   };
 
-  gulp.src([paths.src.root + '/*.hbs'])
+  return gulp.src([paths.src.root + '/*.hbs'])
     .pipe(handlebars(null, opts))
     .on('error', log)
     .pipe(rename({
       extname: '.html',
     }))
     .on('error', log)
-    .pipe(gulp.dest(paths.dist.root))
-    .pipe(browserSync.reload({stream: true}));
-});
-
-/*
-* Bundle all javascript files
-*/
-gulp.task('scripts', ['tslint'], () => {
-  gulp.src(paths.src.typescript)
-    .pipe(tsProject())
-    .pipe(babel({
-      presets: ['env'],
-    }))
-    .pipe(concat('bundle.js'))
-    .on('error', log)
-    .pipe(uglify())
-    .on('error', log)
-    .pipe(gulp.dest(paths.dist.javascript))
-    .pipe(browserSync.reload({stream: true}));
-
-  /*
-  * Uglify JS libs and move to dist folder
-  */
-  gulp.src([paths.src.libs])
-    .pipe(tsLibProject())
-    .pipe(uglify())
-    .on('error', log)
-    .pipe(rename({
-      suffix: '.min',
-    }))
-    .on('error', log)
-    .pipe(gulp.dest(paths.dist.libs))
-    .pipe(browserSync.reload({stream: true}));
-});
-
-gulp.task('images', () => {
-  gulp.src([paths.src.images])
-    .pipe(gulp.dest(paths.dist.images));
-});
-
-gulp.task('files', () => {
-  gulp.src([paths.src.files])
     .pipe(gulp.dest(paths.dist.root));
-});
+}
+exports.templates = templates;
 
-gulp.task('tslint', () => {
-  gulp.src(paths.src.typescript)
+/**
+ * Compile and bundle all typescript files
+ * 
+ */
+function scripts() {
+    return gulp.parallel(
+        () => {
+            gulp.src(paths.src.typescript)
+            .pipe(tsProject())
+            .pipe(babel({
+            presets: ['env'],
+            }))
+            .pipe(concat('bundle.js'))
+            .on('error', log)
+            .pipe(uglify())
+            .on('error', log)
+            .pipe(gulp.dest(paths.dist.javascript))
+        },
+
+        /*
+        * Uglify JS libs and move to dist folder
+        */
+        () => {
+            gulp.src([paths.src.libs])
+            .pipe(tsLibProject())
+            .pipe(uglify())
+            .on('error', log)
+            .pipe(rename({
+            suffix: '.min',
+            }))
+            .on('error', log)
+            .pipe(gulp.dest(paths.dist.libs))
+        }
+    );
+}
+exports.scripts = scripts;
+
+function images() {
+  return gulp.src([paths.src.images])
+    .pipe(gulp.dest(paths.dist.images));
+}
+exports.images = images;
+
+function files() {
+  return gulp.src([paths.src.files])
+    .pipe(gulp.dest(paths.dist.root));
+}
+exports.files = files;
+
+function lint() {
+  return gulp.src(paths.src.typescript)
     .pipe(tslint({
         configuration: './tslint.json',
         formatter: 'prose'
@@ -135,25 +148,21 @@ gulp.task('tslint', () => {
     .pipe(tslint.report({
         summarizeFailureOutput: true
     }))
-})
+}
+exports.lint = lint;
 
-watch(paths.src.images, () => {
-  gulp.start('images');
-});
+function watch() {
+  gulp.watch('src/scss/**/*.scss', styles);
+  gulp.watch(paths.src.typescript, scripts);
+  gulp.watch(paths.src.templates, templates);
+  gulp.watch('dist/**/*.*').on('change', browserSync.reload);
+}
+exports.watch = watch;
 
-watch(paths.src.files, () => {
-  gulp.start('files');
-});
-
-gulp.task('watch', () => {
-  gulp.watch('src/scss/**/*.scss', ['styles']);
-  gulp.watch(paths.src.typescript, ['scripts']);
-  gulp.watch(paths.src.templates, ['templates']);
-});
-
-gulp.task('deploy', () => {
+function deploy() {
   return gulp.src([paths.dist.root + '/**/*'])
     .pipe(ghPages());
-});
+}
+exports.deploy = deploy;
 
-gulp.task('default', ['watch', 'serve', 'images', 'files', 'styles', 'scripts', 'templates']);
+gulp.task('default', gulp.parallel(watch, serve, images, files, styles, scripts, templates));
